532af870e00341a783f4088ad1472928ca038aa7
Firas Altayeb <tayebfiras@gmail.com>
2020-10-21 15:05:42 +0900

    added an app bar and body text

--- Quiz_app/lib/main.dart
+++ Quiz_app/lib/main.dart
@@ -1,13 +1,17 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(Myapp());
-}
+void main() => runApp(Myapp());
 
 class Myapp extends StatelessWidget {
+  @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Text('Hello!'),
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Firas First App'),
+        ),
+        body: Text('This is a default text!'),
+      ),
     );
   }
 }
