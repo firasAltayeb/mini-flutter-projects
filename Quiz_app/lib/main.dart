afd4d0da06ab9b97278f51dabdfcf94e278091ea
Firas Altayeb <tayebfiras@gmail.com>
2020-10-21 15:58:06 +0900

    Added a column inside the body

--- Quiz_app/lib/main.dart
+++ Quiz_app/lib/main.dart
@@ -5,12 +5,32 @@ void main() => runApp(Myapp());
 class Myapp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    var questions = [
+      'what\'s your favorite animal?',
+      'what\'s your favorite color?',
+    ];
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
           title: Text('Firas First App'),
         ),
-        body: Text('This is a default text!'),
+        body: Column(
+          children: [
+            Text('Question'),
+            RaisedButton(
+              child: Text('answer 1'),
+              onPressed: null,
+            ),
+            RaisedButton(
+              child: Text('answer 2'),
+              onPressed: null,
+            ),
+            RaisedButton(
+              child: Text('answer 3'),
+              onPressed: null,
+            ),
+          ],
+        ),
       ),
     );
   }
