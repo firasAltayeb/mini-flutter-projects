7922f530aa856d5bc04c568c40c492816dbcc53c
Firas Altayeb <tayebfiras@gmail.com>
2020-11-25 19:03:11 +0900

    added a date picker widget p4

--- Personal_keep/lib/widgets/new_transactions.dart
+++ Personal_keep/lib/widgets/new_transactions.dart
@@ -15,16 +15,20 @@ class _NewTransactionsState extends State<NewTransactions> {
   DateTime _selectedDate;
 
   void _submitData() {
+    if (_amountController.text.isEmpty) {
+      return;
+    }
     final enteredTitle = _titleController.text;
     final enteredAmount = double.parse(_amountController.text);
 
-    if (enteredTitle.isEmpty || enteredAmount <= 0) {
+    if (enteredTitle.isEmpty || enteredAmount <= 0 || _selectedDate == null) {
       return;
     }
 
     widget.addTx(
-      _titleController.text,
-      double.parse(_amountController.text),
+      enteredTitle,
+      enteredAmount,
+      _selectedDate,
     );
 
     Navigator.of(context).pop();
