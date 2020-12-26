9164515708c20725b9bd72f0feb53988ea37e5db
Firas Altayeb <tayebfiras@gmail.com>
2020-12-26 23:44:08 +0900

    used some dummy data to create categories

--- Meal_track/lib/categories_screen.dart
+++ Meal_track/lib/categories_screen.dart
@@ -1,10 +1,18 @@
 import 'package:flutter/material.dart';
 
+import 'package:Meal_track/Models/dummy_data.dart';
+import 'package:Meal_track/categories_item.dart';
+
 class CategoriesScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return GridView(
-      children: [],
+      children: DUMMY_CATEGORIES
+          .map((catData) => CategoryItem(
+                catData.title,
+                catData.color,
+              ))
+          .toList(),
       gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
           maxCrossAxisExtent: 200,
           childAspectRatio: 1.5,
