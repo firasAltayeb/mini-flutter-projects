410343a737557217e4ebfd6ea9d6e288f93384c3
Firas Altayeb <tayebfiras@gmail.com>
2020-12-27 23:28:20 +0900

    added fonts and used them

--- Meal_track/lib/categories_screen.dart
+++ Meal_track/lib/categories_screen.dart
@@ -9,6 +9,7 @@ class CategoriesScreen extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(title: const Text('DeliMeal')),
       body: GridView(
+        padding: const EdgeInsets.all(25),
         children: DUMMY_CATEGORIES
             .map((catData) => CategoryItem(
                   catData.title,
