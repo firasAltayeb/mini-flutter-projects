4862ff16bcdb36f64153ae8ba936f587f8caaf0e
Firas Altayeb <tayebfiras@gmail.com>
2020-12-29 21:44:28 +0900

    passed data to new page

--- Meal_track/lib/cat_meal_screen.dart
+++ Meal_track/lib/cat_meal_screen.dart
@@ -1,11 +1,16 @@
 import 'package:flutter/material.dart';
 
 class CategoryMealScreen extends StatelessWidget {
+  final String categoryId;
+  final String categoryTitle;
+
+  CategoryMealScreen(this.categoryId, this.categoryTitle);
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('The Recipes'),
+        title: Text(categoryTitle),
       ),
       body: Center(
         child: Text('The recipes for the Catergory'),
--- Meal_track/lib/categories_item.dart
+++ Meal_track/lib/categories_item.dart
@@ -2,16 +2,17 @@ import 'package:Meal_track/cat_meal_screen.dart';
 import 'package:flutter/material.dart';
 
 class CategoryItem extends StatelessWidget {
+  final String id;
   final String title;
   final Color color;
 
-  CategoryItem(this.title, this.color);
+  CategoryItem(this.id, this.title, this.color);
 
   void selectCategory(BuildContext ctx) {
     Navigator.of(ctx).push(
       MaterialPageRoute(
         builder: (_) {
-          return CategoryMealScreen();
+          return CategoryMealScreen(id, title);
         },
       ),
     );
