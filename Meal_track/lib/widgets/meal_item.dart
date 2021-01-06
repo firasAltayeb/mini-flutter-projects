9ffd7a99fc6255220c88451b415e010e3fafdbe0
Firas Altayeb <tayebfiras@gmail.com>
2021-01-06 23:34:44 +0900

    added text on top of meal pictures

--- Meal_track/lib/widgets/meal_item.dart
+++ Meal_track/lib/widgets/meal_item.dart
@@ -33,8 +33,9 @@ class MealItem extends StatelessWidget {
               children: [
                 ClipRRect(
                   borderRadius: BorderRadius.only(
-                      topLeft: Radius.circular(15),
-                      topRight: Radius.circular(15)),
+                    topLeft: Radius.circular(15),
+                    topRight: Radius.circular(15),
+                  ),
                   child: Image.network(
                     imageUrl,
                     height: 250,
@@ -42,6 +43,27 @@ class MealItem extends StatelessWidget {
                     fit: BoxFit.cover,
                   ),
                 ),
+                Positioned(
+                  bottom: 20,
+                  right: 10,
+                  child: Container(
+                    width: 300,
+                    color: Colors.black54,
+                    padding: EdgeInsets.symmetric(
+                      vertical: 5,
+                      horizontal: 20,
+                    ),
+                    child: Text(
+                      title,
+                      style: TextStyle(
+                        fontSize: 26,
+                        color: Colors.white,
+                      ),
+                      softWrap: true,
+                      overflow: TextOverflow.fade,
+                    ),
+                  ),
+                )
               ],
             )
           ],
