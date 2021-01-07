907fe97159ba731d87c5af511ac2d6e4cad6ce3a
Firas Altayeb <tayebfiras@gmail.com>
2021-01-07 23:11:18 +0900

    added more information to the meal list items

--- Meal_track/lib/widgets/meal_item.dart
+++ Meal_track/lib/widgets/meal_item.dart
@@ -1,4 +1,5 @@
 import 'package:Meal_track/models/meal.dart';
+import 'package:Meal_track/screens/meal_detail_screen.dart';
 import 'package:flutter/material.dart';
 
 class MealItem extends StatelessWidget {
@@ -8,19 +9,54 @@ class MealItem extends StatelessWidget {
   final Complexity complexity;
   final Affordability affordability;
 
-  MealItem(
-      {@required this.title,
-      @required this.imageUrl,
-      @required this.duration,
-      @required this.complexity,
-      @required this.affordability});
+  MealItem({
+    @required this.title,
+    @required this.imageUrl,
+    @required this.duration,
+    @required this.complexity,
+    @required this.affordability,
+  });
 
-  void selectMeal() {}
+  String get complexityText {
+    switch (complexity) {
+      case Complexity.Simple:
+        return 'Simple';
+        break;
+      case Complexity.Challenging:
+        return 'Complexity';
+        break;
+      case Complexity.Hard:
+        return 'Hard';
+        break;
+      default:
+        return 'Unkown';
+    }
+  }
+
+  String get affordabilityText {
+    switch (affordability) {
+      case Affordability.Affordable:
+        return 'Affordable';
+        break;
+      case Affordability.Pricey:
+        return 'Pricey';
+        break;
+      case Affordability.Luxurious:
+        return 'Expensive';
+        break;
+      default:
+        return 'Unkown';
+    }
+  }
+
+  void selectMeal(BuildContext context) {
+    Navigator.of(context).pushNamed(MealDetailScreen.routeName);
+  }
 
   @override
   Widget build(BuildContext context) {
     return InkWell(
-      onTap: selectMeal,
+      onTap: () => selectMeal(context),
       child: Card(
         shape: RoundedRectangleBorder(
           borderRadius: BorderRadius.circular(15),
@@ -65,7 +101,35 @@ class MealItem extends StatelessWidget {
                   ),
                 )
               ],
-            )
+            ),
+            Padding(
+                padding: const EdgeInsets.all(20),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  children: [
+                    Row(
+                      children: [
+                        Icon(Icons.schedule),
+                        SizedBox(width: 6),
+                        Text('$duration min')
+                      ],
+                    ),
+                    Row(
+                      children: [
+                        Icon(Icons.work),
+                        SizedBox(width: 6),
+                        Text(complexityText),
+                      ],
+                    ),
+                    Row(
+                      children: [
+                        Icon(Icons.attach_money),
+                        SizedBox(width: 6),
+                        Text(affordabilityText),
+                      ],
+                    )
+                  ],
+                )),
           ],
         ),
       ),
