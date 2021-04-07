d06fec676815b4050b222601579e9cee0fee0394
Firas Altayeb <tayebfiras@gmail.com>
2021-04-07 18:07:17 +0900

    added a cart screen with navigation to it

--- Shop_app/lib/main.dart
+++ Shop_app/lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
+import './screens/cart_screen.dart';
 import './screens/products_overview_screen.dart';
 import './screens/product_detail_screen.dart';
 import './providers/products.dart';
@@ -30,6 +31,7 @@ class MyApp extends StatelessWidget {
           home: ProductsOverviewScreen(),
           routes: {
             ProductDetailScreen.routeName: (ctx) => ProductDetailScreen(),
+            CartScreen.routeName: (ctx) => CartScreen(),
           }),
     );
   }
